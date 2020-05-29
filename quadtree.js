04bdf103a48ec8dbb306f6acee7d38386882d8dd
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2020-05-29 11:20:59 -0400

    feat: added classes for rectangles and quadtrees

--- quadtree.js
+++ quadtree.js
@@ -6,7 +6,39 @@ class Point {
 }
 
 class Rectangle {
-  constructor() {
-    
+  constructor(x, y, w, h) {
+    this.x = x
+    this.y = y 
+    this.w = w
+    this.h = h
+  }
+}
+
+class QuadTree {
+  constructor(boundary, n) {
+    this.boundary = boundary
+    this.capacity = n
+    this.points = []
+    this.divided = false
+  }
+
+  subdivide() {
+    let nw = new Rectangle(this.x + this.w / 2, this.y - this.h/2, this.w/2, this.h/2)
+    let ne = new Rectangle(this.x - this.w / 2, this.y - this.h/2, this.w/2, this.h/2)
+    let sw = new Rectangle(this.x + this.w / 2, this.y + this.h/2, this.w/2, this.h/2)
+    let se = new Rectangle(this.x - this.w / 2, this.y + this.h/2, this.w/2, this.h/2)
+   
+    this.northwest = new QuadTree(nw)
+    this.northeast = new QuadTree(ne)
+    this.southwest = new QuadTree(sw)
+    this.southeast = new QuadTree(se)
+  }
+
+  insert(point) {
+    if(this.points.length < this.capacity){
+      this.points.push(point)
+    } else {
+      this.subdivide()
+    }
   }
 }
